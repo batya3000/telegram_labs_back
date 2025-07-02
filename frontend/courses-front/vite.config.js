bb7f55273a0975d78f4a53e8907e5e7388b0c40a
Anton Kozlov <anton.kozlov@finbeat.tech>
2025-07-02 12:15:42 +0300

    Initial commit

---
+++ frontend/courses-front/vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => {
+          if (path.startsWith('/api/admin')) {
+            return path;
+          }
+          if (
+            path.startsWith('/api/courses') ||
+            path.startsWith('/api/groups') ||
+            path.startsWith('/api/labs')
+          ) {
+            return path.replace(/^\/api/, '');
+          }
+          return path;
+        },
+      },
+    },
+  },
+});
